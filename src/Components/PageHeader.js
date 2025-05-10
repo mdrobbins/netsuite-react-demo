7f741ad9741ac77dd7b6027f89b16015fe8536b2
mrobbins <mdrobbins@mdrnetworks.com>
2025-05-10 16:45:31 -0500

    Remove unused links from page header

--- src/Components/PageHeader.js
+++ src/Components/PageHeader.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import { NavigationLink } from './NavigationLink';
+import {Link} from 'react-router-dom';
+import {NavigationLink} from './NavigationLink';
 
 function PageHeader() {
     return (
@@ -15,38 +15,19 @@ function PageHeader() {
                                 xmlns="http://www.w3.org/2000/svg"
                             >
                                 <title>Airtable</title>
-                                <path d="M11.992 1.966c-.434 0-.87.086-1.28.257L1.779 5.917c-.503.208-.49.908.012 1.116l8.982 3.558a3.266 3.266 0 0 0 2.454 0l8.982-3.558c.503-.196.503-.908.012-1.116l-8.957-3.694a3.255 3.255 0 0 0-1.272-.257zM23.4 8.056a.589.589 0 0 0-.222.045l-10.012 3.877a.612.612 0 0 0-.38.564v8.896a.6.6 0 0 0 .821.552L23.62 18.1a.583.583 0 0 0 .38-.551V8.653a.6.6 0 0 0-.6-.596zM.676 8.095a.644.644 0 0 0-.48.19C.086 8.396 0 8.53 0 8.69v8.355c0 .442.515.737.908.54l6.27-3.006.307-.147 2.969-1.436c.466-.22.43-.908-.061-1.092L.883 8.138a.57.57 0 0 0-.207-.044z" />
+                                <path d="M11.992 1.966c-.434 0-.87.086-1.28.257L1.779 5.917c-.503.208-.49.908.012 1.116l8.982 3.558a3.266 3.266 0 0 0 2.454 0l8.982-3.558c.503-.196.503-.908.012-1.116l-8.957-3.694a3.255 3.255 0 0 0-1.272-.257zM23.4 8.056a.589.589 0 0 0-.222.045l-10.012 3.877a.612.612 0 0 0-.38.564v8.896a.6.6 0 0 0 .821.552L23.62 18.1a.583.583 0 0 0 .38-.551V8.653a.6.6 0 0 0-.6-.596zM.676 8.095a.644.644 0 0 0-.48.19C.086 8.396 0 8.53 0 8.69v8.355c0 .442.515.737.908.54l6.27-3.006.307-.147 2.969-1.436c.466-.22.43-.908-.061-1.092L.883 8.138a.57.57 0 0 0-.207-.044z"/>
                             </svg>
                         </div>
                         <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                            <NavigationLink to="/" label="Search Page" />
-                            <NavigationLink to="/inventory" label="Inventory" />
+                            <NavigationLink to="/" label="Search Page"/>
+                            <NavigationLink to="/inventory" label="Inventory"/>
                             <NavigationLink
                                 to="/purchasing"
                                 label="Purchasing"
                             />
-                            <NavigationLink to="/crm" label="CRM" />
-                            <NavigationLink to="/customer/CUST-1001" label="Customer Details" />
                         </div>
                     </div>
                     <div className="flex items-center">
-                        <div className="shrink-0">
-                            <button
-                                type="button"
-                                className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                            >
-                                <svg
-                                    className="-ml-0.5 size-5"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                    aria-hidden="true"
-                                    data-slot="icon"
-                                >
-                                    <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-                                </svg>
-                                New Contact
-                            </button>
-                        </div>
                         <div className="hidden md:ml-4 md:flex md:shrink-0 md:items-center">
                             <button
                                 type="button"
@@ -80,4 +61,4 @@ function PageHeader() {
     );
 }
 
-export { PageHeader };
+export {PageHeader};
